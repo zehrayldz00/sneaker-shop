f93b14d7fe4cf49eb1a0edb0f60f006b9d7b6710
Zehra Gul Yildiz <zehragul15@gmail.com>
2024-11-15 23:47:14 +0300

    the texts designed cart page created

--- lib/components/shoe_tile.dart
+++ lib/components/shoe_tile.dart
@@ -16,39 +16,71 @@ class ShoeTile extends StatelessWidget {
       decoration: BoxDecoration(
           color: Colors.grey[300],
           borderRadius: BorderRadius.circular(12),
-          boxShadow: [BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            blurRadius: 10,
-            spreadRadius: 2
-          )]
-      ),
-      child: Column( //2:13:55
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                blurRadius: 10,
+                spreadRadius: 2)
+          ]),
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           //shoe pic
           ClipRRect(
-            borderRadius: BorderRadius.circular(13),
+              borderRadius: BorderRadius.circular(13),
               child: Image.asset(shoe.imagePath)),
 
           //description
-          Text(shoe.description, style: TextStyle(color: Colors.grey[600],),),
+          Text(
+            shoe.description,
+            style: TextStyle(
+              color: Colors.grey[600],
+            ),
+          ),
 
           //price + details
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                children: [
-                  // shoe name
-                  Text(shoe.name),
-                  // price
-                  Text(shoe.price),
-              ],),
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // shoe name
+                    Text(
+                      shoe.name,
+                      style: const
+                      TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize:20
+                      ),
+                    ),
+                    const SizedBox(height: 5,),
 
-            // plus button
-              Icon(Icons.add),
-          ],)
+                    // price
+                    Text(
+                      '\$' + shoe.price,
+                      style: const TextStyle(
+                          color: Colors.grey
+                      ),
+                    ),
+                  ],
+                ),
 
+                // plus button
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(12), bottomRight: Radius.circular(12)),
+                  ),
+                  child: const Icon(Icons.add, color: Colors.white),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
---
+++ lib/models/cart.dart
@@ -0,0 +1,13 @@
+class Cart{
+  //list of shoes
+
+  //list of items
+
+  //get list of items for sale
+
+  // get cart
+
+  //add items to cart
+
+  //remove items from cart
+}
