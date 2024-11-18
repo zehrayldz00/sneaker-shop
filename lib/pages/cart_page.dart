5d81bb48ffdf43ceb7b07466a26049f813e41ab4
Zehra Gul Yildiz <zehragul15@gmail.com>
2024-11-18 23:36:27 +0300

    Started cart page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/cart.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Cart"),
-    );
+    return Consumer<Cart>( //2.20.59
+        builder: (context, value, child) => const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+                children: [
+                  Text('My Cart'),
+                ],
+              ),
+        ));
   }
 }
